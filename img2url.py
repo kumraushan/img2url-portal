e577407b7b5f2347d657d1846a8815a7bf7c6bba
Raushan Kumar <kumarraushanrk5@gmail.com>
2020-09-09 16:37:17 +0530

    done changes for multiple images

--- img2url.py
+++ img2url.py
@@ -1,7 +1,5 @@
 import os
-
 from flask import Flask, render_template, request, redirect, send_file, url_for
-
 from awsS3_functions import upload_file,get_url
 
 
@@ -9,27 +7,32 @@ app = Flask(__name__)
 UPLOAD_FOLDER = "uploads"
 BUCKET = 'uploadingimageandgettingurl'
 
-#
-# @app.route('/')
-# def entry_point():
-#     return 'Hello World!'
 
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def storage():
-    #contents = list_files("flaskdrive") , contents=contents
     return render_template('index.html')
 
 
 @app.route("/upload", methods=['POST'])
 def upload():
     if request.method == "POST":
-        f = request.files['file']
-        f.save(f.filename)
-        upload_file(f"{f.filename}", BUCKET)
-        url = get_url(f"{f.filename}", BUCKET)
-        print(url)
-        return render_template('index.html', img_url = url)
+        files = request.files.getlist('files[]')
+        #print(files)
+        url_list = []
+        for file in files:
+            if file and allowed_file(file.filename):
+                #file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+                file.save(file.filename)
+                upload_file(f"{file.filename}", BUCKET)
+                url = get_url(f"{file.filename}", BUCKET)
+                url_list.append(url)
+        #print(url_list)
+        return render_template('index.html', urllist = url_list)
 
 
 if __name__ == '__main__':
